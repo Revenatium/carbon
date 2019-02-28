77c440729b3f7973020e3b146f36dc3d1ce9c927
Raphael Isidro Rodríguez <raphaww@gmail.com>
2019-02-27 20:45:24 -0500

    stickyFooter, opcion de sidebar-widget en listado de rooms

--- source/theme.js
+++ source/theme.js
@@ -10,33 +10,46 @@ import './addons/jquery.sticky';
 import './addons/jquery.magnific-popup';
 import './addons/jquery.validate';
 
-$(document).ready(function($){
+$(document).ready(function ($) {
+   $.extend(true, $.magnificPopup.defaults, {
+      gallery: {
+         tCounter: '%curr%/%total%' // Markup for "1 of 7" counter
+      }
+   });
    let stickySidebar = null;
-   window.applyStickyHeader = function(){
-      if(window.innerWidth >= 768){
-         $('.navbar-sticky').sticky({topSpacing:0});
+   window.applyStickyHeader = function () {
+      if (window.innerWidth >= 768) {
+         $('.navbar-sticky').sticky({ topSpacing: 0 });
       }
       let sidebarWidget = document.getElementById('sidebar-widget');
-      if(window.innerWidth >= 992 && sidebarWidget) {
+      if (window.innerWidth >= 992 && sidebarWidget) {
          let height = ($('.navbar-sticky').outerHeight() | 0) + 32;
          let options = { topSpacing: height };
          stickySidebar = new StickySidebar(sidebarWidget, options);
       }
    }
 
-   $(".navbar-toggler").click(function(){
-      $('#sticky-wrapper').css( "height", "auto");
+   $(".navbar-toggler").click(function () {
+      $('#sticky-wrapper').css("height", "auto");
    });
 
-   $('.photo-gallery').each(function(){
+   $('.photo-gallery').each(function () {
       $(this).magnificPopup({
          delegate: 'a',
+         type: 'image',
+         gallery: { enabled: true }
+      });
+   });
+
+   $('.photo-gallery-thumb').each(function(){
+      $(this).magnificPopup({
+         delegate: '.photo-gallery-thumb-item',
          type: 'image',
          gallery: {enabled:true}
       });
    });
 
-   $('.video-link').each(function(){
+   $('.video-link').each(function () {
       $(this).magnificPopup({
          disableOn: 700,
          type: 'iframe',
@@ -47,30 +60,44 @@ $(document).ready(function($){
       });
    });
 
-   $('.carousel').carousel({interval: 4000});
+   $('.go-to-booker').click(function(){
+      var bookerTop = $('.widget-card').offset().top - $('.navbar-sticky').outerHeight() - 15;
+      $('html, body').animate({scrollTop: bookerTop }, 500);
+   });
+
+   $('.carousel').carousel({ interval: 4000 });
 
    $('.contact-form').validate({
       messages: window.formMessages
    });
 
    $('[data-countdown]').each(function () {
-      var $this = $(this); 
+      var $this = $(this);
       var finalDate = $(this).data('countdown');
       $this.countdown(finalDate, function (event) {
          var totalHours = event.offset.totalDays * 24 + event.offset.hours;
-         if(!event.elapsed && totalHours<=72){
+         if (!event.elapsed && totalHours <= 72) {
             $this.html(event.strftime(totalHours + 'h %Mm %Ss'));
             $this.parent().removeClass('d-none');
          }
       });
    });
 
-   $(window).resize(function() {
+   $(window).resize(function () {
       $('.navbar-sticky').unstick();
-      if(stickySidebar){
+      if (stickySidebar) {
          stickySidebar.destroy();
       }
       window.applyStickyHeader();
+   });
+
+   $(document).on('scroll', function() { 
+      let cardBottom = $('.widget-card').offset().top + $('.widget-card').outerHeight();
+      if(cardBottom < $(document).scrollTop()){ 
+         $('.sticky-bottom').css('maxHeight', 200);
+      }else{
+         $('.sticky-bottom').css('maxHeight', 0); 
+      } 
    });
 
    window.applyStickyHeader();
